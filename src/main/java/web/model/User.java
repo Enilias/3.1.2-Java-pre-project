4297f973f4a4f20d57bc5151fb77d45046d2292e
Enias <dgonssailar34@gmail.com>
2023-11-12 21:58:09 +0300

    Lombok fix

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,9 +1,11 @@
 package web.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,30 +22,6 @@ public class User {
     public User(int id, String name, String surname) {
         this.id = id;
         this.name = name;
-        this.surname = surname;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
         this.surname = surname;
     }
 }
